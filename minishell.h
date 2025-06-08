2048c298fb2c7db5b33cf12983088e26f1ec2250
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-08 17:27:56 -0300

    feat(headerfile): created the minishell.h to the project and include libraries

---
+++ minishell.h
@@ -0,0 +1,13 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <sys/wait.h>
+# include <errno.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
