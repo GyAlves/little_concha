b771941746b4671e9eac06f14e752032cf7d78ff
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-09 20:02:31 -0300

    aux(ft_strlcpy): added to auxiliate ft_strdup

---
+++ Libft/libft/ft_strlcpy.c
@@ -0,0 +1,25 @@
+#include "../../minishell.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	i = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	if (size == 0)
+	{
+		return (j);
+	}
+	while ((i < size - 1) && (src[i] != '\0'))
+	{
+		dst[i] = src[i];
+			i++;
+	}
+	dst[i] = '\0';
+	return (j);
+}
