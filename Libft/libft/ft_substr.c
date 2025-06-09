513c327afdbad88a82bfed02177f51a3999ca6fd
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-09 10:51:27 -0300

    aux(ft_substr): added to auxiliate ft_split

---
+++ Libft/libft/ft_substr.c
@@ -0,0 +1,38 @@
+#include "../../minishell.h"
+
+static char	*allocate_memory(size_t len)
+{
+	char	*buffer;
+
+	buffer = (char *)malloc(len + 1);
+	if (!buffer)
+		return (NULL);
+	return (buffer);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	len_s;
+	char	*buffer;
+	size_t	index;
+
+	if (!s)
+		return (ft_calloc(1, sizeof(char)));
+	len_s = ft_strlen(s);
+	if (start >= len_s)
+		return (ft_calloc(1, sizeof(char)));
+	if (start + len > len_s)
+		len = len_s - start;
+	index = 0;
+	buffer = allocate_memory(len);
+	if (!buffer)
+		return (NULL);
+	while (index < len)
+	{
+		buffer[index] = s[start];
+		index++;
+		start++;
+	}
+	buffer[index] = '\0';
+	return (buffer);
+}
