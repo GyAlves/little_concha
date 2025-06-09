7ffe8c3464f287430276f431b0bb03e809214dff
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-09 13:14:06 -0300

    fix(access): changed condition to access() work properly

--- src/path.c
+++ src/path.c
@@ -4,10 +4,9 @@ void	find_path(int c, char **v, t_command cmd)
 {
 	if (ft_strchr(cmd.args[0], '/'))
 	{
-		if (access(cmd.args[0], X_OK))
+		if (access(cmd.args[0], X_OK) == 0)
 			execvp(cmd.args[0], cmd.args);
 	}
 	else
 		perror("cmd not found");
-	envar_handler(c, v, &cmd);
 }
