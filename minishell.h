7a2484b3ff4ccf4f6a34663172b3b455b2a3a5da
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-09 20:21:01 -0300

    feat(prototype): added new prototypes from header

--- minishell.h
+++ minishell.h
@@ -33,6 +33,8 @@ char	*find_path(char *cmd);
 size_t	ft_strlen(const char *s);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
