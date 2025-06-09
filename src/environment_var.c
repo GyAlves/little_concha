80fc0c8ce7cf8991b5cce9ea8448e5590d30e8f8
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-09 11:59:31 -0300

    changed(envar_handler): function to retrieve environment path

--- src/environment_var.c
+++ src/environment_var.c
@@ -1,25 +1,28 @@
 #include "../minishell.h"
 
-int	envar_handler(int c, char **v, char **envp)
+int	envar_handler(int c, char **v, t_command cmd)//, char **envp)
 {
-	int		i;
-	char 	*envar;
+	//int		i;
+	//char 	*envar;
 
-	i = 0;
+	//i = 0;
 	/*envar = envp[i];
 	while (envar != NULL)
 	{
 		printf("%s\n", envar);
 		i++;
 		envar = envp[i];
-	}*/
+	}
 	envar = getenv("PATH");
 	if (envar == NULL)
 	{
 		printf("command not found: %s\n", v[1]);
 		return (1);
 	}
-	printf("env variables: %s\n", envar);
-
+	printf("env variables: %s\n", envar);*/
+	(char **)cmd = getenv("PATH");
+	if (&cmd == NULL)
+		(char **)cmd = "/bin:/usr/bin";
+	ft_split(&cmd, ':');
 	return (0);
 }
