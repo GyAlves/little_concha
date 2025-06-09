51ca3e6da6707e6ba110237712481b4443beadde
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-09 16:32:36 -0300

    fix(get_path): removed unnecessary free(), changed envar_path type from: 'char *' to: 'char **'

--- src/environment_var.c
+++ src/environment_var.c
@@ -2,29 +2,11 @@
 
 char	**get_path(void)//, char **envp)
 {
-	//int		i;
-	char 	*envar_path;
-
-	//i = 0;
-	/*envar = envp[i];
-	while (envar != NULL)
-	{
-		printf("%s\n", envar);
-		i++;
-		envar = envp[i];
-	}
-	envar = getenv("PATH");
-	if (envar == NULL)
-	{
-		printf("command not found: %s\n", v[1]);
-		return (1);
-	}
-	printf("env variables: %s\n", envar);*/
+	char 	**envar_path;
+	
 	envar_path = getenv("PATH");
 	if (envar_path == NULL)
 		return ("/bin:/usr/bin");
 	ft_split(envar_path, ':');
-	if (!envar_path)
-		free(envar_path);
 	return (envar_path);
 }
