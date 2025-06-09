c5a41ada760c96f57920fc56793c574414f0e880
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-09 20:06:48 -0300

    fix(allocation/dup): fixed allocation and made a dup from paths

--- src/environment_var.c
+++ src/environment_var.c
@@ -8,8 +8,10 @@ char	**get_path(void)
 	path = getenv("PATH");
 	if (path == NULL)
 	{
-		path = "/bin:/usr/bin";
-		ft_calloc(1, sizeof(char *)); //não entendi
+		envar_path = ft_calloc(3, sizeof(char *));
+		envar_path[0] = ft_strdup("/bin");
+		envar_path[1] = ft_strdup("/usr/bin");
+		envar_path[2] = NULL;
 	}
 	else
 		envar_path = ft_split(path, ':');
