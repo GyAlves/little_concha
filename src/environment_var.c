c4ac8554f0c98e4cf8ec44609cc6dfca715c6583
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-10 13:30:52 -0300

    fix(robustness): increased the robustness in the function by passing condition and returning NULL

--- src/environment_var.c
+++ src/environment_var.c
@@ -9,11 +9,21 @@ char	**get_path(void)
 	if (path == NULL)
 	{
 		envar_path = ft_calloc(3, sizeof(char *));
+		if (!envar_path)
+			return (NULL);
 		envar_path[0] = ft_strdup("/bin");
+		if (!envar_path[0])
+			return (NULL);
 		envar_path[1] = ft_strdup("/usr/bin");
+		if (!envar_path[1])
+			return (NULL);
 		envar_path[2] = NULL;
 	}
 	else
+	{
 		envar_path = ft_split(path, ':');
+		if (!envar_path)
+			return (NULL);
+	}
 	return (envar_path);
 }
