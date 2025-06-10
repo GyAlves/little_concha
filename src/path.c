fb78cfd4d25f888780f738a7317d4f6a443e7c06
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-10 13:55:58 -0300

    fix(memory): removed loop that causes double-free, passed a more legible condition to access(), temporarily passes a perror() message to condition, get_path(implementation moved from the begining of the function to the else condition, managed the free(path) to all loop, not just to the success case

--- src/path.c
+++ src/path.c
@@ -7,32 +7,31 @@ char	*find_path(char *cmd)
 	char	*dir;
 	char	*full_path;
 
-	path = get_path();
 	if (ft_strchr(cmd, '/'))
 	{
 		if (access(cmd, X_OK) == 0)
 			return (cmd);
-		else
+		else if (access(cmd, X_OK) == -1)
+		{
+			perror("not executable!\n");
 			return (NULL);
+		}
 	}
 	else
 	{
 		i = 0;
+		path = get_path();
 		while (path[i])
 		{
 			dir = ft_strjoin(path[i], "/");
 			full_path = ft_strjoin(dir, cmd);
 			free(dir);
 			if (access(full_path, X_OK) == 0)
-			{
-				free(path);
 				return (full_path);
-			}
+			free(path);
 			free(full_path);
 			i++;
 		}
 	}
-	while (path)
-		free(path);
-	return (cmd);
+	return (NULL);
 }
