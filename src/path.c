a76f23e95bbb179ea41ab98e709c0dd7796a8960
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-11 21:07:53 -0300

    feat(paths): changed name from functions to become more readable

--- src/path.c
+++ src/path.c
@@ -1,8 +1,8 @@
 #include "../minishell.h"
 
-char	**get_path(void)
+char	**get_envar_path(void)
 {
-	char 	*path;
+	char	*path;
 	char	**envar_path;
 
 	path = getenv("PATH");
@@ -35,16 +35,16 @@ char	**get_path(void)
 	return (envar_path);
 }
 
-static char	*check_dir(char *cmd, char *path)
+static char	*build_and_check_path(char *cmd, char *path_dir)
 {
-	char	*dir;
+	char	*dir_path;
 	char	*full_path;
 
-	dir = ft_strjoin(path, "/");
-	if (!dir)
+	dir_path = ft_strjoin(path_dir, "/");
+	if (!dir_path)
 		return (NULL);
-	full_path = ft_strjoin(dir, cmd);
-	free(dir);
+	full_path = ft_strjoin(dir_path, cmd);
+	free(dir_path);
 	if (!full_path)
 		return (NULL);
 	if (access(full_path, X_OK) == 0)
@@ -53,31 +53,31 @@ static char	*check_dir(char *cmd, char *path)
 	return (NULL);
 }
 
-static char	*dir_path(char *cmd)
+static char	*search_in_paths(char *cmd)
 {
 	int		i;
 	char	*full_path;
 	char	**path;
 
-	path = get_path(); //simple cmd without '/', to find the folder in PATH
+	path = get_envar_path(); //simple cmd without '/', to find the folder in PATH
 	if (!path)
 		return (NULL);
 	i = 0;
 	while (path[i])
 	{
-		full_path = check_dir(cmd, path[i]);
+		full_path = build_and_check_path(cmd, path[i]);
 		if (full_path)
 		{
 			free_matrix(path);
 			return (full_path);
-		};
+		}
 		i++;
 	}
 	free_matrix(path);
 	return (NULL); //no path found
 }
 
-char	*find_path(char *cmd)
+char	*set_path(char *cmd)
 {
 	if (ft_strchr(cmd, '/')) //could be absolute or relative
 	{
@@ -85,5 +85,5 @@ char	*find_path(char *cmd)
 			return (cmd); //is executable
 		return (NULL); //exists but is not executable
 	}
-		return (dir_path(cmd));
+	return (search_in_paths(cmd));
 }
