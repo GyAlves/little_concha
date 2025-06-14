f4751a27a289f5e7c6563219faad952d7d339d5a
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-14 17:56:33 -0300

    fix(error message): changed error message and added a new line

--- src/built-in/bi_pwd.c
+++ src/built-in/bi_pwd.c
@@ -5,11 +5,12 @@ void	bi_pwd(t_minishell *ms)
 	char	*cwd = getcwd(NULL, 0);
 	if (!cwd)
 	{
-		perror("pwd: command not found!\n");
+		perror("pwd");
 		ms->exit_status = 1;
 		return ;
 	}
 	write(1, cwd, ft_strlen(cwd));
+	write(1, "\n", 1);
 	free(cwd);
 	ms->exit_status = 0;
 }
