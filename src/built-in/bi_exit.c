c187b69dbf37e381be4aecc8e5727b792495637c
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-14 15:26:57 -0300

    fix(write issue): changed from perror() to write(), also, removed variable useless

--- src/built-in/bi_exit.c
+++ src/built-in/bi_exit.c
@@ -1,20 +1,16 @@
 #include "../../minishell.h"
 
-void	bi_exit(t_minishell *ms, t_command *cmd)
+void	bi_exit(t_minishell *ms, t_command *cmd, char *prompt)
 {
-	int	recieved_err;
-
-	write(2, "exit\n", 6);
+	write(2, "exit\n", 5);
 	if (cmd->args[1] && cmd->args[2])
 	{
-		perror("exit: too many arguments!\n");
+		write(2, "minishell: exit: too many arguments\n", 36);
 		ms->exit_status = 1;
 		return ;
 	}
 	if (cmd->args[1])
-	{
-		recieved_err = ft_atoi(cmd->args[1]);
-		ms->exit_status = recieved_err;
-	}
-	exit(ms->exit_status);
+		ms->exit_status = ft_atoi(cmd->args[1]);
+	cleanup_n_exit(ms, cmd, prompt);
+
 }
