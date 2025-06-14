006342c154fab60b66c7d5ae0c63661b9fa32e02
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-14 17:55:01 -0300

    fix(cleanup function): passed a function to properly clear and exit from program, also, changed FD from 2 to 1 in exit message

--- src/built-in/bi_exit.c
+++ src/built-in/bi_exit.c
@@ -2,7 +2,7 @@
 
 void	bi_exit(t_minishell *ms, t_command *cmd, char *prompt)
 {
-	write(2, "exit\n", 5);
+	write(1, "exit\n", 5);
 	if (cmd->args[1] && cmd->args[2])
 	{
 		write(2, "minishell: exit: too many arguments\n", 36);
@@ -12,5 +12,4 @@ void	bi_exit(t_minishell *ms, t_command *cmd, char *prompt)
 	if (cmd->args[1])
 		ms->exit_status = ft_atoi(cmd->args[1]);
 	cleanup_n_exit(ms, cmd, prompt);
-
 }
