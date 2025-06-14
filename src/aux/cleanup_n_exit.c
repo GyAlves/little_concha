614559fbbed03662a0b149c4ed565f0c951ca23b
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-14 17:58:33 -0300

    feat(clean and exit): create to aux the bi_exit()

--- src/aux/cleanup_n_exit.c
+++ src/aux/cleanup_n_exit.c
@@ -2,9 +2,15 @@
 
 void	cleanup_n_exit(t_minishell *ms, t_command *cmd, char *prompt)
 {
-	if (cmd->args)
-		free_matrix(cmd->args);
+	if (cmd)
+	{
+		if (cmd->args)
+			free_matrix(cmd->args);
+	}
 	if (prompt)
 		free(prompt);
-	exit(ms->exit_status);
+	if (ms)
+		exit(ms->exit_status);
+	else
+		exit (1);
 }
