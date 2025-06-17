134aafbcb7c2011fbadfc51d964c502123152d71
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-17 17:39:25 -0300

    feat(prototype): added and removed function prototypes in the header

--- minishell.h
+++ minishell.h
@@ -26,22 +26,29 @@ typedef struct s_minishell
 	int		exit_status;
 }			t_minishell;
 
+// src/built-in/bi_cd
+char	*find_env_var(char **envp, char *key);
+void	update_env_var(t_minishell *sh, char* key, char *val);
+void	bi_cd(t_minishell *sh, t_command *cmd);
 // src/built-in/bi_echo
-void	bi_echo(t_minishell *ms, t_command *cmd);
+void	bi_echo(t_minishell *sh, t_command *cmd);
+// src/built-in/bi_env
+void	bi_env(t_minishell *sh, t_command *cmd);
 // src/built-in/bi_exit
-void	bi_exit(t_minishell *ms, t_command *cmd, char *prompt);
+void	bi_exit(t_minishell *sh, t_command *cmd, char *prompt);
 // src/built-in/bi_pwd
-void	bi_pwd(t_minishell *ms);
+void	bi_pwd(t_minishell *sh);
 // src/built-in/built_in
 int		is_builtin(t_command *cmd);
-void	builtin_env(t_minishell *ms, t_command *cmd);
 // src/utils/cleanup_n_exit
-void	cleanup_n_exit(t_minishell *ms, t_command *cmd, char *prompt);
+void	cleanup_n_exit(t_minishell *sh, t_command *cmd, char *prompt);
 // src/utils/main_utils
-int		init_n_exc_cmd(t_minishell *ms, t_command *cmd, char **args, char *prompt);
+char	**read_input(char **prompt);
+int		init_n_exc_cmd(t_minishell *sh, t_command *cmd, \
+		char **args, char *prompt);
 // src/commands
-void	print_cmd_err(char *cmd);
-void	exec_cmd(t_minishell *ms, t_command *cmd);
+//void	print_cmd_err(char *cmd);
+void	exec_cmd(t_minishell *sh, t_command *cmd);
 // src/environment_var
 char	**get_envar_path(void);
 // src/path
@@ -51,6 +58,7 @@ size_t	ft_strlen(const char *s);
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	ft_putstr_fd(char *s, int fd);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
