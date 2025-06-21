edc13ac523c81c551ebf87526ebefa38cb437479
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-21 19:18:37 -0300

    update(): removed 'cd -' from the function, also, created a new function called cd_envar_home() in the bi_cd, that treats the change dir to HOME, (modularization in progress)

--- src/built-in/bi_cd.c
+++ src/built-in/bi_cd.c
@@ -8,19 +8,74 @@ static void	print_cd_no_file_nor_dir(char *path)
 	ft_putstr_fd(": No such file or directory\n", 2);
 }
 
+static char	*cd_envar_home(t_minishell *sh)
+{
+	char *target;
+
+	target = find_envar(sh->envp, "HOME"); //uses HOME
+	if (!target)
+	{
+		ft_putstr_fd("cd: HOME not set\n", 2);
+		return (NULL);
+	}
+	target += 5; //advances to after "HOME=" taking only the value
+	if (!target[0]) //if it's empty, returns error
+	{
+		ft_putstr_fd("cd: HOME is empty\n", 2);
+		return (NULL);
+	}
+	return (target);
+}
+
+void	bi_cd(t_minishell *sh, t_command *cmd)
+{
+	char	*cwd; //stores the curr dir
+	char	*target; //target dir
+
+	if (!cmd->args[1]) //just cd to go to HOME
+	{
+		target = cd_envar_home(sh);
+		if (!target)
+		{
+			sh->exit_status = 1;
+			return ;
+		}
+	}
+	else
+		target = cmd->args[1]; //dir to go
+	if (chdir(target) == -1) //tryes to change to target dir
+	{
+		print_cd_no_file_nor_dir(target);
+		sh->exit_status = 1;
+		return  ;
+	}
+	cwd = getcwd(NULL, 0); //get the absolute path
+	if (!cwd)
+	{
+		sh->exit_status = 1;
+		return ;
+	}
+	update_envar(sh, "PWD", cwd); //update PWD
+	free(cwd);
+	sh->exit_status = 0;
+}
+
+/*
+
 void	bi_cd(t_minishell *sh, t_command *cmd)
 {
 	char	*cwd;
 	char	*target;
 	char	*oldpwd;
-
+//can be a function
 	oldpwd = getcwd(NULL, 0); //saves current dir
 	if (!oldpwd)
 	{
 		sh->exit_status = 1;
 		return ;
 	}
-	if (!cmd->args[1]) //just cd
+//can be a function
+	if (!cmd->args[1]) //just cd to go to HOME
 	{
 		target = find_envar(sh->envp, "HOME"); //uses HOME
 		if (!target)
@@ -30,8 +85,8 @@ void	bi_cd(t_minishell *sh, t_command *cmd)
 			sh->exit_status = 1;
 			return ;
 		}
-		target += 5;
-		if (!target[0])
+		target += 5; //advances to after "HOME=" taking only the value
+		if (!target[0]) //if it's empty, returns error
 		{
 			ft_putstr_fd("cd: HOME is empty\n", 2);
 			free(oldpwd);
@@ -39,7 +94,8 @@ void	bi_cd(t_minishell *sh, t_command *cmd)
 			return ;
 		}
 	}
-	else if (ft_strncmp(cmd->args[1], "-",  2) == 0) //cd -
+//can be a function or can be removed
+	else if (ft_strncmp(cmd->args[1], "-",  2) == 0) //cd - returns to the latest dir
 	{
 		target = find_envar(sh->envp, "OLDPWD"); //uses OLDPWD
 		if (!target)
@@ -91,3 +147,5 @@ void	bi_cd(t_minishell *sh, t_command *cmd)
 	free(cwd);
 	sh->exit_status = 0;
 }
+
+*/
