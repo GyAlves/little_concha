429b5217f3f7ab13438aa454f0a5e205f4441ba2
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-27 12:32:23 -0300

    fix(delete_envar modularization): fixed the logit inside the function and started to modularize it creating new aux functions, like envar_unset_new_list()

--- src/built-in/bi_unset.c
+++ src/built-in/bi_unset.c
@@ -1,44 +1,46 @@
 #include "../../minishell.h"
 
+static char	**envar_unset_new_list(t_minishell *sh, char *key)
+{
+	int		count;
+	char	**new_envp;
+	
+	if (!find_envar(sh->envp, key))
+		return (NULL); //não faz nada
+	count = ft_arrlen(sh->envp);
+	new_envp = ft_calloc(count, sizeof(char *)); //cria um novo array
+	return (new_envp);
+}
+
 static void	delete_envar(t_minishell *sh, char *key)
 {
 	int		i;
-	int		len;
-	char	*equal;
-	int		count;
+	int		j;
 	char	**new_envp;
 
-	i = 1;
-	while (key[i])
-	{
-		equal = ft_strchr(key[i], '=');
-		if (!is_valid_id(key[i]))
-		{
-			sh->exit_status = 0;
-			return ;
-		}
-		if (equal)
-			key = ft_strndup(key[i], equal - key[i]);
-		else
-			key = ft_strdup(key[i]);
-			i++;
-	}
 	i = 0;
-	if (!find_envar(sh->envp, key))
-		return ; //não faz nada
-	else
+	j = 0;
+	new_envp = envar_unset_new_list(sh, key);
+	if (!new_envp)
+		return ;
+	while (sh->envp[i])
 	{
-		count = ft_arrlen(sh->envp);
-		new_envp = ft_calloc(count - 1, sizeof(char *)); //cria um novo array
-	}
-	while (ft_strncmp(sh->envp[i], key, ft_strlen(key)) && sh->envp[i][len] == '=')
-	{
-		len = 0;
-		
-		new_envp[i] = ft_strdup(sh->envp[i]);
+		if (ft_strncmp(sh->envp[i], key, ft_strlen(key)) \
+			|| sh->envp[i][ft_strlen(key)] != '=')
+		{
+			new_envp[j] = ft_strdup(sh->envp[i]);
+			if (!new_envp[j])
+			{
+				while (j > 0)
+					free(new_envp[--j]);
+				free(new_envp);
+				return ;
+			}
+			j++;
+		}
 		i++;
-		len++;
 	}
+	free_matrix(sh->envp);
 	sh->envp = new_envp;
 }
 
@@ -53,20 +55,23 @@ void	bi_unset(t_minishell *sh, t_command *cmd)
 {
 	int		i;
 
-	i = 1;
 	if (!cmd->args[1])
+	{
+		sh->exit_status = 0;
 		return ;
+	}
+	i = 1;
+	sh->exit_status = 0;
 	while (cmd->args[i])
 	{
 		if (!is_valid_id(cmd->args[i]))
 		{
-			print_unset_err(cmd->args);
+			print_unset_err(cmd->args[i]);
 			sh->exit_status = 1;
 		}
-		delete_envar(sh, cmd->args[i]);
-		sh->exit_status = 0;
+		else
+			delete_envar(sh, cmd->args[i]);
 		i++;
 	}
-	sh->exit_status = 0;
 }
 
