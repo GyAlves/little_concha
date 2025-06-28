ec09cae61c47728df48e6f6d28fc7ff74aa9c39a
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-28 15:33:33 -0300

    fix(update_envar): handles val parameter more carefully

--- src/environment_variables/envar_management.c
+++ src/environment_variables/envar_management.c
@@ -22,22 +22,33 @@ static char	**append_envar(t_minishell *sh, char *envar_entry, int count)
 		free(new_envp);
 		return (NULL);
 	}
-	new_envp[count] = envar_entry;
+	new_envp[count] = ft_strdup(envar_entry);
+	if (!new_envp[count])
+	{
+		free_matrix(new_envp);
+		return (NULL);
+	}
 	new_envp[count + 1] = NULL;
 	return (new_envp);
 }
 
 static int	update_curr_envar(t_minishell *sh, char *envar_entry, char *key)
 {
-	int	i;
+	int		i;
+	char	*tmp;
 
 	i = 0;
 	while (sh->envp[i])
 	{
 		if (ft_strncmp(sh->envp[i], key, ft_strlen(key)) == 0 \
-		&& sh->envp[i][ft_strlen(key)] == '=')
+		&& (sh->envp[i][ft_strlen(key)] == '=' \
+		|| sh->envp[i][ft_strlen(key)] == '\0'))
 		{
-			sh->envp[i] = envar_entry;
+			tmp = sh->envp[i];
+			sh->envp[i] = ft_strdup(envar_entry);
+			free(tmp);
+			if (!sh->envp[i])
+				return (0);
 			return (1);
 		}
 		i++;
@@ -51,18 +62,24 @@ void	update_envar(t_minishell *sh, char *key, char *val)
 	char	*envar_entry;
 	char	**new_envp;
 
-	envar_entry = create_envar_entry(key, val);
+	if (!key)
+		return ;
+	if (val)
+		envar_entry = create_envar_entry(key, val);
+	else
+		envar_entry = ft_strdup(key);
 	if (!envar_entry)
 		return ;
 	if (update_curr_envar(sh, envar_entry, key))
-		return ;
-	count = ft_arrlen(sh->envp);
-	new_envp = append_envar(sh, envar_entry, count);
-	if (!new_envp)
 	{
 		free(envar_entry);
 		return ;
 	}
+	count = count_init_envar(sh->envp);
+	new_envp = append_envar(sh, envar_entry, count);
+	free(envar_entry);
+	if (!new_envp)
+		return ;
 	free_matrix(sh->envp);
 	sh->envp = new_envp;
 }
