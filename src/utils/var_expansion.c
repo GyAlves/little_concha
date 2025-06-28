25b0d986c7a3ccd21263b94b13c0a905bba38359
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-28 15:57:35 -0300

    feat(var expansion): handles var expansion with '$'

---
+++ src/utils/var_expansion.c
@@ -0,0 +1,24 @@
+#include "../../minishell.h"
+
+char	*expand_varriable(t_minishell *sh, char *str)
+{
+	char	*key;
+	char	*val;
+	char	*res;
+
+	if (str[0] != '$')
+	{
+		res = ft_strdup(str);
+		return (res);
+	}
+	key = str + 1;
+	val = find_envar(sh->envp, key);
+	if (val)
+		res = ft_strdup(val);
+	else
+		res = ft_strdup("");
+	if (!res)
+		return (NULL);
+		return (res);
+}
+
