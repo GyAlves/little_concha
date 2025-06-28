d649710645c4577398ed5a039857ca0b48fd7a51
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-28 16:17:40 -0300

    update(bi_export): created handle_n_flag() that handles the -n flag in bi_echo, started the modularization to receive variable expansion later

--- src/built-in/bi_echo.c
+++ src/built-in/bi_echo.c
@@ -1,17 +1,25 @@
 #include "../../minishell.h"
 
+static int	handle_n_flag(char **args, int *index)
+{
+	int	n_flag;
+
+	n_flag = 0;
+	while (args[*index] && ft_strncmp(args[*index], "-n", 3) == 0)
+	{
+		n_flag = 1;
+		(*index)++;
+	}
+	return (n_flag);
+}
+
 void	bi_echo(t_minishell *sh, t_command *cmd)
 {
 	int	index;
 	int	n_flag;
 
 	index = 1;
-	n_flag = 0;
-	if (cmd->args[1] && ft_strncmp(cmd->args[1], "-n", 3) == 0)
-	{
-		n_flag = 1;
-		index++;
-	}
+	n_flag = handle_n_flag(cmd->args, &index);
 	while (cmd->args[index])
 	{
 		write(1, cmd->args[index], ft_strlen(cmd->args[index]));
@@ -19,7 +27,7 @@ void	bi_echo(t_minishell *sh, t_command *cmd)
 			write(1, " ", 1);
 		index++;
 	}
-	if (n_flag == 0)
+	if (!n_flag)
 		write(1, "\n", 1);
 	sh->exit_status = 0;
 }
