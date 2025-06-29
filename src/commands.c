6c60399be27b305ddb7929ab6e945963a8399a03
Guildguy <fabiolljunior23@gmail.com>
2025-06-29 13:21:57 -0300

    update(commands): changed print error funtion from: write() to: ft_putstr_fd()

--- src/commands.c
+++ src/commands.c
@@ -3,7 +3,7 @@
 static void	print_cmd_err(char *cmd)
 {
 	ft_putstr_fd("minishell: <", 2);
-	write(2, cmd, ft_strlen(cmd));
+	ft_putstr_fd(cmd, 2);
 	ft_putstr_fd(">: command not found!\n", 2);
 }
 
