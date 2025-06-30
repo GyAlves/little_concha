850b83fa8d5c0163fa8e0ba6c24ff6b4b649da7d
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-06-30 17:58:09 -0300

    update(taking off the parser from it): removed the parser of the function and passed properly to another functions

--- src/utils/shell_utils.c
+++ src/utils/shell_utils.c
@@ -2,25 +2,19 @@
 
 char	**read_input(char **prompt)
 {
-	char	**args;
-
 	*prompt = readline("type> ");
 	if (!*prompt)
 		return (NULL);
 	add_history(*prompt);
-	args = ft_split(*prompt, ' ');
-	return (args);
+	return (lexer(*prompt));
 }
 
 int	init_n_exc_cmd(t_minishell *sh, t_command *cmd, char **args, char *prompt)
 {
 	int	status;
 
-	cmd->args = args;
-	cmd->input_file = NULL;
-	cmd->output_file = NULL;
-	cmd->piped = 0;
-	cmd->append = 0;
+	if (!parse_cmd(sh, cmd, args))
+		return (1);
 	if (is_builtin(cmd))
 		status = dispatch_builtin(sh, cmd, prompt);
 	else
