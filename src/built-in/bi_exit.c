6ed9a2fb119f6d6df1226aca8e35063347cf1ec1
Guildguy <fabiolljunior23@gmail.com>
2025-06-30 11:46:34 -0300

    fix(mem. leak): free memory properly when exits the program

--- src/built-in/bi_exit.c
+++ src/built-in/bi_exit.c
@@ -9,10 +9,11 @@ static void	print_num_arg_required(t_command *cmd)
 
 void	bi_exit(t_minishell *sh, t_command *cmd, char *prompt)
 {
-	write(1, "exit\n", 5);
+	(void)prompt;
+	ft_putstr_fd("exit\n", 1);
 	if (cmd->args[1] && cmd->args[2])
 	{
-		ft_putstr_fd("env: too many arguments\n", 2);
+		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
 		sh->exit_status = 1;
 		return ;
 	}
@@ -22,9 +23,8 @@ void	bi_exit(t_minishell *sh, t_command *cmd, char *prompt)
 		{
 			print_num_arg_required(cmd);
 			sh->exit_status = 2;
-			cleanup_n_exit(sh, cmd, prompt);
+			return ;
 		}
 		sh->exit_status = ft_atoi(cmd->args[1]);
 	}
-	cleanup_n_exit(sh, cmd, prompt);
 }
