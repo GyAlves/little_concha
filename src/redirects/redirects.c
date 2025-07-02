6fda40d4e25b5777b4e268727faae488df48fbda
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-01 21:57:19 -0300

    update(redir output): added function parse_redir(): that gets the number of times that the '>' shows, alloc memory to the redir, updates the struct, and after find the '>', try to assume and verify if exists a file, and apply_redir(): creates a fd and to each redir, do something diff

--- src/redirects/redirects.c
+++ src/redirects/redirects.c
@@ -15,6 +15,42 @@ int	parse_redir(t_command *cmd, char **args)
 		i++;
 	}
 	redir = ft_calloc(count + 1, sizeof(t_redirect));
+	if (!redir)
+		return (0);
 	cmd->redirects = redir;
-	//preciso armazenar na struct qnt. de vezes que o ">" apareceu
+	cmd->redir_count = count;
+	i = 0;
+	count = 0;
+	while (args[i])
+	{
+		if (ft_strcmp(args[i], ">") == 0)
+		{
+			if (!args[i + 1])
+				return (0); //without files
+			redir[count].type = ft_strdup(">");
+			redir[count].filename = ft_strdup(args[i + 1]);
+			if (!redir[count].type || !redir[count].filename)
+				return (0); // alloc err
+			count++;
+			i += 2;
+		}
+		else
+			i++;
+	}
+	return (1);
+}
+
+int	apply_redir(t_redirect *redir)
+{
+	int	fd;
+
+	if (ft_strcmp(redir->type, ">") == 0)
+		fd = open(redir->filename, O_WRONLY | O_CREAT);
+	else
+		return (0);
+	if (fd < 0)
+		return (0);
+	dup2(fd, STDOUT_FILENO);
+	close (fd);
+	return (1);
 }
