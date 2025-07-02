7682c128d65bf85757a57989fc0fb6395c256baa
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-01 21:06:07 -0300

    update(parse_redir): started to implement the '>' count logic in the function

--- src/redirects/redirects.c
+++ src/redirects/redirects.c
@@ -2,5 +2,19 @@
 
 int	parse_redir(t_command *cmd, char **args)
 {
-	//começar com o output ">"
+	int			i;
+	int			count;
+	t_redirect	*redir;
+
+	i = 0;
+	count = 0;
+	while (args[i])
+	{
+		if (ft_strcmp(args[i], ">") == 0)
+			count++;
+		i++;
+	}
+	redir = ft_calloc(count + 1, sizeof(t_redirect));
+	cmd->redirects = redir;
+	//preciso armazenar na struct qnt. de vezes que o ">" apareceu
 }
