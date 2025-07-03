cfc8f832017ac266a672b3c8d572e95c95e5a1a5
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-03 20:51:47 -0300

    fix(mem. release): fixed the return point in the function and relase the mem

--- src/redirects/redirects.c
+++ src/redirects/redirects.c
@@ -31,7 +31,10 @@ static char	**remove_redir(char **args, int *n_count)
 		{
 			n_args[count] = ft_strdup(args[i]);
 			if (!n_args[count])
+			{
+				free_matrix(n_args);
 				return (NULL);
+			}
 			count++;
 			i++;
 		}
@@ -67,11 +70,11 @@ int	parse_redir(t_command *cmd, char **args)
 		if (ft_strcmp(args[i], ">") == 0 || ft_strcmp(args[i], "<") == 0)
 		{
 			if (!args[i + 1])
-				return (0); //without files
+				return (0);
 			redir[count].type = ft_strdup(args[i]);
 			redir[count].filename = ft_strdup(args[i + 1]);
 			if (!redir[count].type || !redir[count].filename)
-				return (0); // alloc err
+				return (0);
 			count++;
 			i += 2;
 		}
@@ -81,6 +84,7 @@ int	parse_redir(t_command *cmd, char **args)
 	n_args = remove_redir(args, &i);
 	if (!n_args)
 		return (0);
+	free_matrix(args);
 	cmd->args = n_args;
 	return (1);
 }
@@ -88,19 +92,31 @@ int	parse_redir(t_command *cmd, char **args)
 int	apply_redir(t_redirect *redir)
 {
 	int	fd;
+	int	std_save;
 
 	if (ft_strcmp(redir->type, ">") == 0)
+	{
 		fd = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		std_save = dup(STDOUT_FILENO);
+	}
 	else if (ft_strcmp(redir->type, "<") == 0)
+	{
 		fd = open(redir->filename, O_RDONLY);
+		std_save = dup(STDIN_FILENO);
+	}
 	else
 		return (0);
-	if (fd < 0)
+	if (fd < 0 || std_save < 0)
 		return (0);
 	if (ft_strcmp(redir->type, "<") == 0)
 		dup2(fd, STDIN_FILENO);
 	else
 		dup2(fd, STDOUT_FILENO);
-	close (fd);
+	close(fd);
+	if (ft_strcmp(redir->type, "<") == 0)
+		dup2(std_save, STDIN_FILENO);
+	else
+		dup2(std_save, STDOUT_FILENO);
+	close(std_save);
 	return (1);
 }
