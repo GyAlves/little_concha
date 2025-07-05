fd9914476d53c2b070cc4befad79017dd3f4e649
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-05 17:13:20 -0300

    update(name): changed from: parse_cmd, to: parse_n_init_cmd

--- src/parser_n_lexer/lexer_n_parser.c
+++ src/parser_n_lexer/lexer_n_parser.c
@@ -12,17 +12,13 @@ char	**lexer(char *input)
 	return (tokens);
 }
 
-int	parse_cmd(t_minishell *sh, t_command *cmd, char **args)
+int	parse_n_init_cmd(t_minishell *sh, t_command *cmd, char **args)
 {
 	(void)sh;
 	if (!args || !args[0])
 		return (0);
 	cmd->args = args;
-	cmd->input_file = NULL;
-	cmd->output_file = NULL;
-	cmd->delimiter = NULL;
 	cmd->piped = 0;
-	cmd->append = 0;
 	cmd->redirects = NULL;
 	cmd->redir_count = 0;
 	if (!args || ! args[0])
