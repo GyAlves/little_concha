6cd100e8c2e63066665bc882e8fc601b3add2a43
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-06 20:18:03 -0300

    update(get_redir_type): updated to get HEREDOC

--- src/utils/fd_utils.c
+++ src/utils/fd_utils.c
@@ -28,11 +28,13 @@ t_redir_type	get_redir_type(char *str)
 {
 	if (!str)
 		return (INVALID);
-	if (ft_strcmp(str, ">") == 0)
+	if (!ft_strcmp(str, ">"))
 		return (R_OUT);
-	else if (ft_strcmp(str, "<") == 0)
-		return (R_IN);
-	else if (ft_strcmp(str, ">>") == 0)
+	if (!ft_strcmp(str, ">>"))
 		return (APPEND);
+	if (!ft_strcmp(str, "<"))
+		return (R_IN);
+	if (!ft_strcmp(str, "<<"))
+		return (HEREDOC);
 	return (INVALID);
 }
