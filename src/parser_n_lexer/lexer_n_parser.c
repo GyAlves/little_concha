296935cd104ff3a22836d7feaa986c6177d0c942
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-06 16:57:42 -0300

    feat(modularization): passed parse_redir() to lexer_n_parser.c

--- src/parser_n_lexer/lexer_n_parser.c
+++ src/parser_n_lexer/lexer_n_parser.c
@@ -12,6 +12,31 @@ char	**lexer(char *input)
 	return (tokens);
 }
 
+int	parse_redir(t_command *cmd, char **args)
+{
+	int				n_count;
+	char			**n_args;
+
+	cmd->redir_count = count_redirs(args);
+	cmd->redirects = ft_calloc(cmd->redir_count + 1, sizeof(t_redirect));
+	if (!cmd->redirects)
+		return (0);
+	if (!fill_redirs(cmd, args))
+	{
+		free(cmd->redirects);
+		return (0);
+	}
+	n_args = filter_n_rm_redir(args, &n_count);
+	if (!n_args)
+	{
+		free(cmd->redirects);
+		return (0);
+	}
+	free_matrix(args);
+	cmd->args = n_args;
+	return (1);
+}
+
 int	parse_n_init_cmd(t_minishell *sh, t_command *cmd, char **args)
 {
 	(void)sh;
