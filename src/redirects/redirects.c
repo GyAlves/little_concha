5db366eb1dd244d76a6a0aa94239e20bfef6fd3c
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-06 18:15:30 -0300

    update(append_redir): added append function to apply_redir()

--- src/redirects/redirects.c
+++ src/redirects/redirects.c
@@ -24,11 +24,25 @@ static int	output_redir(t_redirect *redir)
 	return (1);
 }
 
+static int	append_redir(t_redirect *redir)
+{
+	int	fd;
+
+	fd = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd < 0)
+		return (0);
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (1);
+}
+
 int	apply_redir(t_redirect *redir)
 {
 	if (redir->type == R_IN)
 		return (input_redir(redir));
 	else if (redir->type == R_OUT)
 		return (output_redir(redir));
+	else if (redir->type == APPEND)
+		return (append_redir(redir));
 	return (0);
 }
