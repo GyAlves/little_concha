cddb6a7b3a83d614bba9920c753866ff248aea46
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-07 15:06:37 -0300

    feat(heredoc): file created to implement heredoc logic

---
+++ src/redirects/heredoc.c
@@ -0,0 +1,7 @@
+#include "../../minishell.h"
+
+//heredoc permite passar a STDIN diretamente no terminal até uma palavra delimitadora ex: cat << FNSH
+//criar um imput temporario, a partir de STDIN
+// detectar '<<' \/
+//detectar o delimitador FNSH e escrever até ele ser encontrado
+//salva as linhas em um arquivo temporario
