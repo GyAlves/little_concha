f15a28c366fc89b39b354814a79034fea27820cb
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-07 16:11:50 -0300

    update(write_till_delimiter): function created to call readline and loops throught the call until it reaches the delimiter

--- src/redirects/heredoc.c
+++ src/redirects/heredoc.c
@@ -1,7 +1,35 @@
 #include "../../minishell.h"
 
 //heredoc permite passar a STDIN diretamente no terminal até uma palavra delimitadora ex: cat << FNSH
-//criar um imput temporario, a partir de STDIN
+//criar um input temporario, a partir de STDIN
+//vamos:
 // detectar '<<' \/
-//detectar o delimitador FNSH e escrever até ele ser encontrado
-//salva as linhas em um arquivo temporario
+//detectar o delimitador FNSH \/
+//escrever até o delimitador ser encontrado \/
+//criar um arquivo temporario
+//salvar as linhas em um arquivo temporario
+
+static int	write_till_delimiter(int fd, char *delimiter, char **envp)
+{
+	char	*line;
+	char	*expanded;
+
+	while (6)
+	{
+		line = readline("> ");
+		if (!line || ft_strcmp(line, delimiter) == 0)
+		{
+			if (!line)
+				free(line);
+			return (1);
+		}
+		expanded = replace_variables(NULL, line);
+		free(line);
+		if (!expanded)
+			return (0);
+		write(fd, expanded, ft_strlen(expanded));
+		write(fd, "\n", 1);
+		free(expanded);
+	}
+	return (0);
+}
