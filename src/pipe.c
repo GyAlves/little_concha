2e1928d35d32ebc3d42f5064fda63b3bdde62ea1
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-08 18:43:27 -0300

    feat(create_pipe): function thats alloc memory and creates a pipe

---
+++ src/pipe.c
@@ -0,0 +1,24 @@
+#include "../minishell.h"
+
+static int	create_pipes(int **pipes, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		pipes[i] = malloc(2 * sizeof(int));
+		if (!pipes[i] || pipe(pipes[i]) == -1)
+		{
+			while (i-- > 0)
+			{
+				close(pipes[i][0]);
+				close(pipes[i][1]);
+				free(pipes[i]);
+			}
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
