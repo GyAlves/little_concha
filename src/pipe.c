09c243d7b63d4623cd5fd1394e0a71c8d018259d
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-08 18:46:21 -0300

    feat(close_pipe): function created to iterate throught the function and free the pipes

--- src/pipe.c
+++ src/pipe.c
@@ -22,3 +22,18 @@ static int	create_pipes(int **pipes, int count)
 	}
 	return (1);
 }
+
+static int	close_pipes(int **pipes, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i-- > 0)
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+		free(pipes[i]);
+		i++;
+	}
+}
+
