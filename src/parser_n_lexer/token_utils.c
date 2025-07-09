2b4339e74bcd44b769040fd827f19f10aef384fe
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-09 16:02:11 -0300

    feat(token): updating token_utils and send functions to it for modularization

---
+++ src/parser_n_lexer/token_utils.c
@@ -0,0 +1,26 @@
+#include "../../minishell.h"
+
+t_redir_type	get_redir_type(char *str)
+{
+	if (!str)
+		return (INVALID);
+	if (!ft_strcmp(str, ">"))
+		return (R_OUT);
+	if (!ft_strcmp(str, ">>"))
+		return (APPEND);
+	if (!ft_strcmp(str, "<"))
+		return (R_IN);
+	if (!ft_strcmp(str, "<<"))
+		return (HEREDOC);
+	return (INVALID);
+}
+
+int	is_pipe(char *str)
+{
+	return (str && ft_strcmp(str, "|") == 0);
+}
+
+int	is_redir(char *str)
+{
+	return (get_redir_type(str) != INVALID);
+}
