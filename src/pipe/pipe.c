18e44bb562ed1dfcff2dc73be40e2aa079698e8e
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-08 21:16:11 -0300

    update(pipe): almost done

---
+++ src/pipe/pipe.c
@@ -0,0 +1,24 @@
+#include "../../minishell.h"
+
+int	handle_pipes(t_minishell *sh, t_command *cmd, int cmd_count)
+{
+	int			i;
+	t_pipe_data	data;
+
+	data.cmd_count = cmd_count;
+	if (!setup_pipes(&data, cmd_count))
+	{
+		free(data.pipes);
+		free(data.pids);
+		return (0);
+	}
+	i = 0;
+	while (i < cmd_count)
+	{
+		fork_n_redirect_pipe(sh, &cmd[i], &data, i);
+		i++;
+	}
+	close_pipes(&data);
+	wait_pipe_child(&data, sh);
+	return (1);
+}
