44fc129b305d71c6a16d5da2902baa5ee776ace8
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-09 16:00:26 -0300

    feat(parser): updating parser_utils and send functions to it for modularization

---
+++ src/parser_n_lexer/parser_utils.c
@@ -0,0 +1,59 @@
+#include "../../minishell.h"
+
+int	count_cmd_args(char **args)
+{
+	int		i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	while (args[i])
+	{
+		if (get_redir_type(args[i]) != INVALID)
+			i += 2;
+		else
+		{
+			count++;
+			i++;
+		}
+	}
+	return (count);
+}
+
+static char	**cpy_cmd_args(char **args, char **n_args)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (args[i])
+	{
+		if (get_redir_type(args[i]) != INVALID)
+			i += 2;
+		else
+		{
+			n_args[j] = ft_strdup(args[i]);
+			if (!n_args[j])
+			{
+				free_matrix(n_args);
+				return (NULL);
+			}
+			i++;
+			j++;
+		}
+	}
+	return (n_args);
+}
+
+char	**filter_n_rm_redir(char **args, int *n_count)
+{
+	char	**n_args;
+
+	*n_count = count_cmd_args(args);
+	n_args = ft_calloc(*n_count + 1, sizeof(char *));
+	if (!n_args)
+		return (NULL);
+	n_args = cpy_cmd_args(args, n_args);
+	return (n_args);
+}
