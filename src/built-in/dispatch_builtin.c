210bdf1b375376552f7c5fd51d770e735181bfca
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-11 11:58:06 -0300

    update(function parameter): changed calling from exit in dispatch_builtin aux and the orchestrator function

--- src/built-in/dispatch_builtin.c
+++ src/built-in/dispatch_builtin.c
@@ -1,26 +1,26 @@
 #include "../../minishell.h"
 
 static int	bi_from_cd_until_exit(t_minishell *sh, \
-	t_command *cmd, char *prompt, char **args)
+	t_command *cmd, char *prompt)
 {
-	if (ft_strncmp(cmd->args[0], "cd", 3) == 0)
+	if (ft_strcmp(cmd->args[0], "cd") == 0)
 	{
 		bi_cd(sh, cmd);
 		return (sh->exit_status);
 	}
-	else if (ft_strncmp(cmd->args[0], "echo", 5) == 0)
+	else if (ft_strcmp(cmd->args[0], "echo") == 0)
 	{
 		bi_echo(sh, cmd);
 		return (sh->exit_status);
 	}
-	else if (ft_strncmp(cmd->args[0], "env", 4) == 0)
+	else if (ft_strcmp(cmd->args[0], "env") == 0)
 	{
 		bi_env(sh, cmd);
 		return (sh->exit_status);
 	}
-	else if (ft_strncmp(cmd->args[0], "exit", 5) == 0)
+	else if (ft_strcmp(cmd->args[0], "exit") == 0)
 	{
-		bi_exit(sh, cmd, prompt, args);
+		bi_exit(sh, cmd, prompt, NULL);
 		return (sh->exit_status);
 	}
 	return (1);
@@ -28,17 +28,17 @@ static int	bi_from_cd_until_exit(t_minishell *sh, \
 
 static int	bi_from_export_until_unset(t_minishell *sh, t_command *cmd)
 {
-	if (ft_strncmp(cmd->args[0], "export", 7) == 0)
+	if (ft_strcmp(cmd->args[0], "export") == 0)
 	{
 		bi_export(sh, cmd);
 		return (sh->exit_status);
 	}
-	else if (ft_strncmp(cmd->args[0], "pwd", 4) == 0)
+	else if (ft_strcmp(cmd->args[0], "pwd") == 0)
 	{
 		bi_pwd(sh);
 		return (sh->exit_status);
 	}
-	else if (ft_strncmp(cmd->args[0], "unset", 6) == 0)
+	else if (ft_strcmp(cmd->args[0], "unset") == 0)
 	{
 		bi_unset(sh, cmd);
 		return (sh->exit_status);
@@ -50,28 +50,28 @@ int	is_builtin(t_command *cmd)
 {
 	if (!cmd->args[0])
 		return (0);
-	if (ft_strncmp(cmd->args[0], "env", 4) == 0)
+	if (ft_strcmp(cmd->args[0], "env") == 0)
 		return (1);
-	if (ft_strncmp(cmd->args[0], "echo", 5) == 0)
+	if (ft_strcmp(cmd->args[0], "echo") == 0)
 		return (1);
-	if (ft_strncmp(cmd->args[0], "exit", 5) == 0)
+	if (ft_strcmp(cmd->args[0], "exit") == 0)
 		return (1);
-	if (ft_strncmp(cmd->args[0], "pwd", 4) == 0)
+	if (ft_strcmp(cmd->args[0], "pwd") == 0)
 		return (1);
-	if (ft_strncmp(cmd->args[0], "cd", 3) == 0)
+	if (ft_strcmp(cmd->args[0], "cd") == 0)
 		return (1);
-	if (ft_strncmp(cmd->args[0], "export", 7) == 0)
+	if (ft_strcmp(cmd->args[0], "export") == 0)
 		return (1);
-	if (ft_strncmp(cmd->args[0], "unset", 6) == 0)
+	if (ft_strcmp(cmd->args[0], "unset") == 0)
 		return (1);
 	return (0);
 }
 
-int	dispatch_builtin(t_minishell *sh, t_command *cmd, char *prompt, char **args)
+int	dispatch_builtin(t_minishell *sh, t_command *cmd, char *prompt)
 {
 	int	return_val;
 
-	return_val = bi_from_cd_until_exit(sh, cmd, prompt, args);
+	return_val = bi_from_cd_until_exit(sh, cmd, prompt);
 	if (return_val <= 0)
 		return (return_val);
 	return_val = bi_from_export_until_unset(sh, cmd);
