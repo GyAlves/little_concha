9ddf212d490818824a7fa0aac0898ec18c94c670
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-07-22 09:37:56 -0300

    update(comment): removed from signal file

--- src/signal/signal.c
+++ src/signal/signal.c
@@ -12,10 +12,6 @@
 
 #include "minishell.h"
 
-//ctrl + C: SIGINT: exibe um novo prompt em uma nova linha, sai com o status 130.
-//ctrl + \: encerra uma execução causando core dump, não imprimir ^\. sai com o status 131.
-//ctrl + D: SIGQUIT: sai do shell se estiver vazio, sobre um texto ele deleta os char, serve como (DELETE), não altera o status de saida
-
 void	handle_sigint(int sig)
 {
 	(void)sig;
